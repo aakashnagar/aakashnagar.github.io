11a321c102435b3bd206061c587aa082215673d2
aakashnagar <31370168+aakashnagar@users.noreply.github.com>
2020-08-12 09:43:09 -0500

    Update index.js
    Fixed popup issue by changing https to http

--- js/index.js
+++ js/index.js
@@ -47,7 +47,7 @@ function plot(f, t) {
     }
     if (document.getElementById("district").checked == false) {
         d3.json("https://cs498finalproject.s3.us-east-2.amazonaws.com/states.json", function (json) {
-            d3.json("https://api.nightlights.io/months/" + interval + "/states", function (j) {
+            d3.json("http://api.nightlights.io/months/" + interval + "/states", function (j) {
                 if (j == null || (f[0] > t[0] && f[1] <= t[1])) {
                     alert("Select only between Jan 1993 and  Dec 2013");
                     location.reload();
